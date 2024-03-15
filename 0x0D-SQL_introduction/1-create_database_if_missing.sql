26564584f542b49c6567b1eb6a598ae7a9fc3b6b
Andy Miguel <miguelandy875@gmail.com>
2024-03-16 01:39:49 +0200

    multiple project file upload

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- a script that creates the database hbtn_0c_0 in your MySQL server.
+-- a script that creates the database hbtn_0c_0 in MySQL server.
+-- If the database hbtn_0c_0 already exists, the script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
