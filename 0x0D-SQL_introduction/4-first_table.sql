fa57694da86c9f02b18d3197f65fe4efe4ba9e3b
Andy Miguel <miguelandy875@gmail.com>
2024-03-16 00:11:38 +0000

    syntax error fixing on file 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,8 @@
 -- a script that creates a table called first_table 
---in the current database in MySQL server.
+-- in the current database in MySQL server.
 -- with id INT and name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS
+first_table (
     id INT,
     name VARCHAR(256)
 );
